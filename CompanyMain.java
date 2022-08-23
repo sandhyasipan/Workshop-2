c5f65ea754ac71ae2cc61607d99c7d7c7e265fc9
sandhyasipan <sandhyaswain70@gmail.com>
2022-08-23 19:46:21 +0530

    added to master

---
+++ CompanyMain.java
@@ -0,0 +1,10 @@
+package com.bridgelab;
+
+public class CompanyMain {
+    public static void main(String args[]){
+        EmployeeDetails emp = new EmployeeDetails();
+        emp.setCompanyname("Emma Watsons");
+        emp.setDipartment("IT");
+        emp.setEmployeeDetails();
+    }
+}
